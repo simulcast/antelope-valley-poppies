ffe92f335db529f3d6828acc95e271eb84601c6b
simulcast <thisissimulcast@gmail.com>
2020-04-21 20:48:16 -0700

    changed init state to false

--- client/src/Poppies.jsx
+++ client/src/Poppies.jsx
@@ -19,7 +19,7 @@ export default class Poppies extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      started: true
+      started: false
     }
 
     this.handleClick = this.handleClick.bind(this)
