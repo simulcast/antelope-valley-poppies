fe0bd3ec2cb2ab15c429994fa9bcf3cb2d753e77
simulcast <thisissimulcast@gmail.com>
2020-04-24 15:25:18 -0700

    changed about text

--- client/src/About.jsx
+++ client/src/About.jsx
@@ -57,10 +57,10 @@ const About = (props) => {
         <p>when it's sunny, they open up and reach towards the light. when it's cloudy, they turn inwards.</p>
         <p>it's usually nice to visit with a friend, but this year, we're left alone.</p>
         <p>knowing this, the california parks department set up a webcam. it's beautiful, but it's quiet.</p>
-        <p><span class="accent">just like the poppies, the score here responds to its surroundings.</span></p>
-        <p>the more of us there are, the sunnier it becomes.</p>
+        <p><span class="accent">just like the poppies, the sounds here respond to their surroundings.</span></p>
+        <p>the more of us there are, the less desolate it becomes.</p>
         <br></br>
-        <p>design + dev by <a href="https://tristanfriedbergrodman.com" rel="noopener noreferrer" target="_blank">tristan friedberg rodman</a>. sound by <a href="https://nevercontent.org/" rel="noopener noreferrer" target="_blank">noah klein</a>.</p>
+        <p>design + dev by <a href="https://tristanfriedbergrodman.com" rel="noopener noreferrer" target="_blank">tristan friedberg rodman</a>. sounds by <a href="https://nevercontent.org/" rel="noopener noreferrer" target="_blank">noah klein</a>.</p>
       </AboutPopUp>
     )
   }
